519c56f0e3d40f2f64e400c605cdefa2416f9e52
AaronJin <aaron.merino@mail.utoronto.ca>
2019-05-24 12:23:25 -0400

    poj to tailo conversion

--- pojtotailo.js
+++ pojtotailo.js
@@ -1,6 +1,7 @@
 
 
 function poj_to_tailo(text) {
+    var accent = '[́̀̂]'
     var conv = {
         "e" :  "i",
         "E" : "I",
@@ -8,8 +9,40 @@ function poj_to_tailo(text) {
         "O" : "U"
     }
 
-    text = text.replace(new RegExp('([eE])(' + '\u030D' + '?k)'), (match, p1, p2, offset, string) => {
-        return conv[p1].concat(p2);
+    function convert(match, p1, p2, offset, string) {
+        return conv[p1] + p2;
+    }
+
+    text = text.replace('#', '')
+
+    text = text.normalize('NFD');
+
+    text = text.replace('\u4ED4', '\u00E1')
+    text = text.replace(new RegExp('\(([^\)]+)\)'), (match, p1, offset, string) => {
+        return p1 +'-' + p1 + '-' + p1
     });
+
+    text = text.replace(new RegExp('([eE])(' + '\u030D' + '?k)'), convert);
+
+    text = text.replace(new RegExp('([eE])(' + accent + '?ng)'), convert);
+
+    text = text.replace(new RegExp('([oO])(' + accent + '?[ae])'), convert);
+
+    text = text.replace(new RegExp('([uU])(' + accent + '?[aei])'), '$1$3$2')
+    text = text.replace(new RegExp('([eE])(' + accent + '?)(re)'), '$1$3$2')
+    text = text.replace(new RegExp('([oO])(' + accent + '?)'), '$1o')
+    text = text.replace(new RegExp('(h?)\U207F'), 'nn$1')
+    text = text.replace(new RegExp('([cC]h)'), (match, p1, offset, string) => {
+        if (match == "ch") {
+            return "ts";
+        } else if (match == "Ch") {
+            return "Ts"
+        }
+    });
+
+    text = text.replace(new RegExp('/([^\s])'), ' / $1')
+
+    text = text.normalize('NFC');
+
     return text
 }
